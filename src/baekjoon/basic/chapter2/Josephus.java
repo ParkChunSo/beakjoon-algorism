e79cacd4c2ddc2934e157ae4c067207ba6c3fb15
ParkChunSo <cnsth123@gmail.com>
2019-03-08 23:43:47 +0900

    조세퍼스 문제

---
+++ src/baekjoon/basic/chapter2/Josephus.java
@@ -0,0 +1,38 @@
+package baekjoon.basic.chapter2;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ * 조세퍼스 문제
+ */
+
+public class Josephus {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LinkedList<Integer> q = new LinkedList<>();
+        LinkedList<Integer> answer = new LinkedList<>();
+        int n = scanner.nextInt(), m = scanner.nextInt();
+        int cnt = 1, tmp;
+
+        for(int i = 1 ; i <= n ; i++)
+            q.add(i);
+
+        while(!q.isEmpty()){
+            tmp = q.poll();
+            if(cnt % m == 0)
+                answer.offer(tmp);
+            else
+                q.offer(tmp);
+
+            if(cnt == q.size())
+                cnt = 0;
+            cnt++;
+        }
+        StringBuilder s = new StringBuilder();
+        while(!answer.isEmpty()) {
+            s.append(answer.poll());
+        }
+        System.out.println(s.toString());
+    }
+}
